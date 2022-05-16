dc780428a841802a7e79db8d49ef02303eb1c7b0
Andrey Tatarikov <atatarikov@gmail.com>
2022-05-16 21:48:59 +0300

    izm2

--- hw.py
+++ hw.py
@@ -1,4 +1,4 @@
-ooo = {"city": "Москва", "temperature": "20"}
+ooo = {"city": "Москва1", "temperature": "20"}
 print(ooo)
-ooo["temperature"] = str(int(ooo["temperature"])-5)
+ooo["temperature"] = str(int(ooo["temperature"])-5) 
 print(ooo)
